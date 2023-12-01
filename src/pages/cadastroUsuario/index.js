c554390d2fc27d26091baa65094fa8d2d57341b5
BlenoFerraz <130615815+BlenoFerraz@users.noreply.github.com>
2023-11-30 21:39:04 -0300

    iniciamos cadastro de usuario

---
+++ src/pages/cadastroUsuario/index.js
@@ -0,0 +1,33 @@
+import '../../pages/global.css';
+import Menu from '../../componentes/menu'
+import{ FiFilePlus }from "react-icons/fi";
+import {Link} from 'react-router-dom';
+import Head from '../../componentes/Head';
+
+export default function Cadastrousuario(){
+
+     return(
+        <div className="dashboard-container">
+      
+            <div className='menu'>
+                <Menu />
+            </div>
+                <div className='principal'>
+                    <Head title="Cadastro de Usuário" />
+                <div>
+                    <Link className='btn-novo'>Cancelar</Link>
+                </div>
+                <form>
+                    <input type='text' placeholder='Digite o nome do usuário' />
+                    <input type='email' placeholder='Digite o email' />
+                    <input type='password' placeholder='Digite a senha' />
+                    <button>Salvar</button>
+                    <button>Cancelar</button>
+                </form>
+
+            </div>
+        </div>
+
+   )
+
+}
